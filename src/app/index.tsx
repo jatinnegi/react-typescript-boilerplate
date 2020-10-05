29da1b4d5fdc5e2c8a493e6270959df862e2857a
Jatin Negi <jatin.07star@gmail.com>
2020-10-05 21:35:11 +0530

    First commit

---
+++ src/app/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { hot } from "react-hot-loader";
+import "./index.css";
+
+const App = hot(module)(() => {
+  return <h1>React App</h1>;
+});
+
+export default App;
